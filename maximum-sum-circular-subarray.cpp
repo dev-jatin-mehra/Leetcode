cf42235a5715406c896fc8156440e32dcf0e5660
Jatin Mehra <dev.jatin.singh@gmail.com>
2025-07-31 22:46:27 +0530

    Update maximum-sum-circular-subarray.cpp

--- maximum-sum-circular-subarray.cpp
+++ maximum-sum-circular-subarray.cpp
@@ -4,16 +4,6 @@ using namespace std;
 class Solution
 {
 private:
-    void rotate(vector<int> &arr)
-    {
-        int start_num = arr[0];
-        for (int i = 1; i < arr.size(); i++)
-        {
-            arr[i - 1] = arr[i];
-        }
-        arr[arr.size() - 1] = start_num;
-    }
-
     int kadanes(vector<int> &arr)
     {
         int current_max = arr[0], global_max = arr[0];
@@ -25,18 +15,26 @@ private:
         return global_max;
     }
 
+    int kadanes_min(vector<int> &arr)
+    {
+        int current_min = arr[0], global_min = arr[0];
+        for (int i = 1; i < arr.size(); i++)
+        {
+            current_min = min(arr[i], current_min + arr[i]);
+            global_min = min(global_min, current_min);
+        }
+        return global_min;
+    }
+
 public:
     int maximumCircularSumSubarray(vector<int> &arr)
     {
-        int result = -1e4; //-10000
-        for (int i = 0; i < arr.size() - 1; i++)
-        {
-            rotate(arr);
-            int sum = kadanes(arr);
-            result = max(result, sum);
-        }
+        int Tsum = accumulate(arr.begin(), arr.end(), 0);
 
-        return result;
+        int minSum = kadanes_min(arr); // find the min_Sum, Sub it with total sum get the circular max_Sum, compare with maxSum of actual array
+        int maxSum = kadanes(arr);     // Proper kadanes
+
+        return maxSum > 0 ? max(Tsum - minSum, maxSum) : maxSum; // for case -> {-1,-1,-1}
     }
 };
 
