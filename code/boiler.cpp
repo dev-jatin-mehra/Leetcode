bf2c3f01726f99630043afdc54d78093fca1825f
Jatin Mehra <dev.jatin.singh@gmail.com>
2025-08-01 22:45:44 +0530

    Create boiler.cpp

---
+++ code/boiler.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+};
+
+int main()
+{
+    Solution obj;
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    return 0;
+}
